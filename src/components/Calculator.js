4fd8eeeef8090ca3b02ed8ec8953524f0418075b
Muhmmd-Usama <mummdusama@gmail.com>
2022-11-02 15:57:46 +0200

    Create Routing components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,39 +15,42 @@ function Calculator() {
   };
 
   return (
-    <div className="calculator">
-      <span className="display" onChange={update}>
-        {data.total}
-        {data.operation}
-        {data.next}
-      </span>
-      <div className="all-keys">
-        <div className="white-key">
-          <div className="functions">
-            <button onClick={click} value="AC" type="button" className="key remove"> AC </button>
-            <button onClick={click} value="+/-" type="button" className="key sign"> ± </button>
-            <button onClick={click} value="%" type="button" className="key percent"> % </button>
+    <div className="container d-flex p-5 m-auto">
+      <h1>Let&#39;s Do Some Math</h1>
+      <div className="calculator">
+        <span className="display" onChange={update}>
+          {data.total}
+          {data.operation}
+          {data.next}
+        </span>
+        <div className="all-keys">
+          <div className="white-key">
+            <div className="functions">
+              <button onClick={click} value="AC" type="button" className="key remove"> AC </button>
+              <button onClick={click} value="+/-" type="button" className="key sign"> ± </button>
+              <button onClick={click} value="%" type="button" className="key percent"> % </button>
+            </div>
+            <div className="numbers">
+              <button onClick={click} value="0" type="button" className="key num-0"> 0 </button>
+              <button onClick={click} value="." type="button" className="key dot"> . </button>
+              <button onClick={click} value="1" type="button" className="key num-1"> 1 </button>
+              <button onClick={click} value="2" type="button" className="key num-2"> 2 </button>
+              <button onClick={click} value="3" type="button" className="key num-3"> 3 </button>
+              <button onClick={click} value="4" type="button" className="key num-4"> 4 </button>
+              <button onClick={click} value="5" type="button" className="key num-5"> 5 </button>
+              <button onClick={click} value="6" type="button" className="key num-6"> 6 </button>
+              <button onClick={click} value="7" type="button" className="key num-7"> 7 </button>
+              <button onClick={click} value="8" type="button" className="key num-8"> 8 </button>
+              <button onClick={click} value="9" type="button" className="key num-9"> 9 </button>
+            </div>
           </div>
-          <div className="numbers">
-            <button onClick={click} value="0" type="button" className="key num-0"> 0 </button>
-            <button onClick={click} value="." type="button" className="key dot"> . </button>
-            <button onClick={click} value="1" type="button" className="key num-1"> 1 </button>
-            <button onClick={click} value="2" type="button" className="key num-2"> 2 </button>
-            <button onClick={click} value="3" type="button" className="key num-3"> 3 </button>
-            <button onClick={click} value="4" type="button" className="key num-4"> 4 </button>
-            <button onClick={click} value="5" type="button" className="key num-5"> 5 </button>
-            <button onClick={click} value="6" type="button" className="key num-6"> 6 </button>
-            <button onClick={click} value="7" type="button" className="key num-7"> 7 </button>
-            <button onClick={click} value="8" type="button" className="key num-8"> 8 </button>
-            <button onClick={click} value="9" type="button" className="key num-9"> 9 </button>
+          <div className="operators">
+            <button onClick={click} value="÷" type="button" className="key key-operators"> ÷ </button>
+            <button onClick={click} value="x" type="button" className="key key-operators"> x </button>
+            <button onClick={click} value="-" type="button" className="key key-operators"> - </button>
+            <button onClick={click} value="+" type="button" className="key key-operators"> + </button>
+            <button onClick={click} value="=" type="button" className="key key-operators"> = </button>
           </div>
-        </div>
-        <div className="operators">
-          <button onClick={click} value="÷" type="button" className="key key-operators"> ÷ </button>
-          <button onClick={click} value="x" type="button" className="key key-operators"> x </button>
-          <button onClick={click} value="-" type="button" className="key key-operators"> - </button>
-          <button onClick={click} value="+" type="button" className="key key-operators"> + </button>
-          <button onClick={click} value="=" type="button" className="key key-operators"> = </button>
         </div>
       </div>
     </div>
